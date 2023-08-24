b65b2c42f35d78eec8c5544dae97975d1d1e753b
Hc-5514 <tbd05050@naver.com>
2023-08-24 23:23:20 +0900

    refact: AppConfig 스프링 기반으로 변경
    순수 자바 코드 -> Spring 으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,10 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,21 +13,26 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 // 생성자 주입
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
-	private MemberRepository memberRepository() {
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
 	}
 }
