31468aaa6d22535e78f71d039c7e7108af7cc843
Hc-5514 <tbd05050@naver.com>
2023-08-27 13:30:14 +0900

    test: @ComponentScan 과 @Component

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,6 +3,9 @@ package hello.core.member;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
 	private static Map<Long, Member> store = new ConcurrentHashMap<>(); // 동시성 이슈 때문에 일반 HashMap 이 아닌, ConcurrentHashMap 을 사용해야 한다.
